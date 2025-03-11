093a53157139c41b7113545aa9f4fd84713f8db0
Lê Hạnh Hải Đăng <110990682+lehanhhaidang@users.noreply.github.com>
2025-03-11 19:41:01 +0700

    App Bar

---
+++ frontend/src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Logout from '@mui/icons-material/Logout'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title='Maiden Dev'>
+        <IconButton
+          onClick={handleClick}
+          size='small'
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-menu-profiles' : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://lh3.googleusercontent.com/a/ACg8ocLIYmMla2BuVPxdC8zYRARfHkt80n8NWy2c_rCb7GcSDW731bF-fw=s288-c-no'
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id='basic-menu-profiles'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profiles'
+        }}
+      >
+        <MenuItem>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize='small' />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize='small' />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize='small' />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -4,8 +4,8 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
@@ -18,6 +18,58 @@ const theme = extendTheme({
       palette: {
         primary: cyan,
         secondary: orange
+      }
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#16a085',
+            borderRadius: '8px'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.main
+          },
+          '& fieldset': {
+            borderWidth: '1px !important'
+          }
+        })
       }
     }
   }
