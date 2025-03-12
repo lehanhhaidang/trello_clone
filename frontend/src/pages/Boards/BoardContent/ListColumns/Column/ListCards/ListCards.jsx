48189f3398ed84fc4b9d5819dbcd87da39b8ec89
Lê Hạnh Hải Đăng <110990682+lehanhhaidang@users.noreply.github.com>
2025-03-12 22:24:34 +0700

    Build FE Board Content

---
+++ frontend/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,47 @@
+import Card from './Card/Card'
+
+import Box from '@mui/material/Box'
+
+function ListCards() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        p: '0 5px',
+        m: '0 5px',
+        flexDirection: 'column',
+        gap: 1,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        maxHeight: (theme) =>
+          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${
+            theme.trello.columnHeaderHeight
+          } - ${theme.trello.columnFooterHeight})`,
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ced0da',
+          borderRadius: '8px'
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: '#bfc2cf',
+          borderRadius: '8px'
+        }
+      }}
+    >
+      <Card />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+    </Box>
+  )
+}
+
+export default ListCards
